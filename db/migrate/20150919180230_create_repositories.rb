7d718bfcb9f0c7d7d9500ebb2e5e81ec7426433f
Chris Olszewski <chrisdolszewski@gmail.com>
2015-09-19 13:52:11 -0500

    i done messed up

--- db/migrate/20150919180230_create_repositories.rb
+++ db/migrate/20150919180230_create_repositories.rb
@@ -2,7 +2,7 @@ class CreateRepositories < ActiveRecord::Migration
   def change
     create_table :repositories do |t|
       t.string :name
-      t.reference :user
+      t.references :user
 
       t.timestamps null: false
     end
