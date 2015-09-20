d77301f3500212ec42e03e9f6b3a55e52cfe8ce8
Haxonek <thomash1996@live.com>
2015-09-19 22:17:42 -0500

    Minor changes to index

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,6 @@ ActiveRecord::Schema.define(version: 20150920022800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-  enable_extension "pg_stat_statements"
-  enable_extension "unaccent"
 
   create_table "repo_files", force: :cascade do |t|
     t.string   "url"
@@ -41,12 +38,24 @@ ActiveRecord::Schema.define(version: 20150920022800) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
     t.string   "provider"
     t.string   "uid"
     t.string   "token"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
