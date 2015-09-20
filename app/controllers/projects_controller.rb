97758a1c01a92ed72a0e750c65f8d2b4c13493fb
Chris Olszewski <chrisdolszewski@gmail.com>
2015-09-19 22:56:22 -0500

    this is the commit of reading stuff

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,25 @@
 class ProjectsController < ApplicationController
-  before_action :load_project, only: [:show, :update]
+  before_action :load_project, only: [:update]
+  before_action :octokit_client, only: [:index, :create]
 
   def index
+    @repositories = @client.repositories
   end
 
-  def show
+  def create
+    repository = Repository.create(gh_name: params[:gh_name], enabled: true, user: current_user)
+    @client.create_hook(
+                        params[:gh_name],
+                        'web',
+                        {
+                          url: "#{request.url.split('?').first}/#{repository.id}",
+                          content_type: 'json'
+                        },
+                        {
+                          events: ['push'],
+                          active: true
+                        })
+    redirect_to :back
   end
 
   def update
@@ -20,5 +35,9 @@ class ProjectsController < ApplicationController
   private
   def load_project
     @project = Repository.find(params[:id])
+  end
+
+  def octokit_client
+    @client = Octokit::Client.new(:access_token => current_user.token)
   end
 end
